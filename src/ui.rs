0ccf7b669f6283cd05fa4310a899064c4ddd56fd
Kasper Nyhus <kanyhus@gmail.com>
2025-09-16 21:32:03 +0200

    initial commit
    template: even-driven-async
    https://github.com/ratatui/templates

---
+++ src/ui.rs
@@ -0,0 +1,57 @@
+use ratatui::widgets::ListState;
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Constraint, Layout, Rect},
+    style::{Color, Modifier, Style},
+    widgets::{Block, List, StatefulWidget, Widget},
+};
+
+use crate::app::App;
+
+pub const RIGHT_ARROW: &str = "▶ ";
+
+impl Widget for &App {
+    /// Renders the user interface widgets.
+    ///
+    // This is where you add new widgets.
+    // See the following resources:
+    // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
+    // - https://github.com/ratatui/ratatui/tree/master/examples
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [top, middle, bottom] = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .areas(area);
+
+        let title = Block::default()
+            .title(" Lazylog ")
+            .title_alignment(Alignment::Center)
+            .style(Style::default().bg(Color::Indexed(237)));
+
+        let footer = Block::default()
+            .title(" Press 'q' to quit ")
+            .title_alignment(Alignment::Center)
+            .style(Style::default().bg(Color::Indexed(237)));
+
+        let items = [
+            "INFO: REW V5.31.3 running Arch Linux JRE 1.8.0_432 64-bit on linux 6.11.9-arch1-1",
+            "INFO: Disabling use of BlendComposite due to internal error, waterfalls will not show overlaid cursor",
+            "SEVERE: Read audio data timed out from PCM: pipewire",
+            "Nov 22, 2024 7:39:21 PM roomeqwizard.ID$1 paintComponent",
+            "javax.sound.sampled.LineUnavailableException: line with format PCM_SIGNED 48000.0 Hz, 32 bit, stereo, 8 bytes/frame, little-endian not supported.",
+        ];
+
+        let mut list_state = ListState::default();
+        list_state.select_first();
+
+        let log_list = List::new(items)
+            .highlight_symbol(RIGHT_ARROW)
+            .highlight_style(Style::default().add_modifier(Modifier::BOLD));
+
+        title.render(top, buf);
+        StatefulWidget::render(log_list, middle, buf, &mut list_state);
+        footer.render(bottom, buf);
+    }
+}
